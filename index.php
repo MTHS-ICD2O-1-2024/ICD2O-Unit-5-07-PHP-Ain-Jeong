ee19217b28c5b75aae77a325ee06d01c9207a753
Ain-Jeong <ain.jeong@stu.ocsb.ca>
2025-05-07 20:27:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,55 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Sum of n natural numbers, with PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Ain Jeong" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-light_green.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Sum of n natural numbers, with PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Sum of n natural numbers, with PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/formula.png" alt="Formula image" width="500" />
+      </div>
+      <br />
+      <div class="page-content-guide">Enter the number that is going to be n:</div>
+      <div class="page-content">
+        <form action="./answer.php" method="GET">
+          <div class="mdl-textfield mdl-js-textfield">
+            <input class="mdl-textfield__input" type="text" pattern="-?[0-9]*(\.[0-9]+)?" name="user-number" />
+            <label class="mdl-textfield__label" for="user-number">n...</label>
+            <span class="mdl-textfield__error">Input is not a number!</span>
+          </div>
+          <br />
+      </div>
+      <br />
+      <!-- Accent-colored raised button with ripple -->
+      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+        type="submit">
+        Calculate
+      </button>
+      </form>
+  </div>
+  </main>
+  </div>
+</body>
+
 </html>
